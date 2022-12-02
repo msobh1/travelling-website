a77f108c826ded4c86aad144339752b58878e9d6
Mahmoud Sobh <msobh71@gmail.com>
2022-12-02 15:53:27 +0200

    11

--- myApp/app.js
+++ myApp/app.js
@@ -10,9 +10,9 @@ const uri = "mongodb://localhost:27017";
 const databaseName = "MyDB";
 MongoClient.connect(uri, { useNewUrlParser: true }, (error, client) => {
   if (error) {
-    return console.log("Connection failed for some reason");
+    return console.log("Connection failed");
   }
-  console.log("Connection established - All well");
+  console.log("Connection established");
   db = client.db(databaseName);
 });
 //session 
@@ -26,9 +26,10 @@ var store =new MongoDBSession({
 }); 
 
 app.use(session({
-   secret: 'some secret',
+   secret: 'secret',
    resave:false,
    saveUninitialized: false,
+   cookie: {expires:(3 * 86400 * 1000)},
    store: store 
  })
 );
@@ -66,7 +67,7 @@ app.get('/registration',function(req,res){
      return res.redirect('registration');
   }else{
     const hashedPsw = await bcrypt.hash(password, 12);
-    db.collection("users").insertOne({username:username ,password:hashedPsw});
+    db.collection("users").insertOne({username:username ,password:hashedPsw,wantToGo:[]});
     res.redirect('/'); 
   }
  });
@@ -82,44 +83,52 @@ app.post('/',async(req,res)=>{
   }else{
     var passMatch= await bcrypt.compare(password,user.password);
     if(!passMatch){
-      return res.redirect('login');
+      return res.redirect('/');
     }else{
+      db.collection("users").updateOne({username: username},{$push: { wantToGo: "habebe" }})
       req.session.isAuth = true;
       res.render('home');
     }
   }
 });
 
-app.get('/hiking',isAuth, (reg, res) => {
+app.get('/hiking',isAuth, (req, res) => {
   res.render('hiking');
 });
-app.get('/cities',isAuth, (reg, res) => {
+app.get('/cities',isAuth, (req, res) => {
   res.render('cities');
 });
-app.get('/islands',isAuth, (reg, res) => {
+app.get('/islands',isAuth, (req, res) => {
   res.render('islands');
 });
-app.get('/wanttogo',isAuth, (reg, res) => {
+app.get('/wanttogo',isAuth, (req, res) => {
   res.render('wanttogo');
 });
-app.get('/inca',isAuth, (reg, res) => {
+app.get('/inca',isAuth, (req, res) => {
   res.render('inca');
 });
-app.get('/annapurna',isAuth, (reg, res) => {
+app.get('/annapurna',isAuth, (req, res) => {
   res.render('annapurna');
 });
-app.get('/paris',isAuth, (reg, res) => {
+app.get('/paris',isAuth, (req, res) => {
   res.render('paris');
 });
-app.get('/rome',isAuth, (reg, res) => {
+app.get('/rome',isAuth, (req, res) => {
   res.render('rome');
 });
-app.get('/bali',isAuth, (reg, res) => {
+app.get('/bali',isAuth, (req, res) => {
   res.render('bali');
 });
-app.get('/santorini',isAuth, (reg, res) => {
+app.get('/santorini',isAuth, (req, res) => {
   res.render('santorini');
 });
 
+app.post('/search',isAuth, (req, res) => {
+  res.render('searchresults');
+});
+app.get('/endpoint',isAuth, (req, res) => {
+  res.render('wanttogo');
+  console.log(req.url.split("?"));
+});
 
 app.listen(4000);
